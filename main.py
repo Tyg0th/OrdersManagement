14da0a93a79c3fb3b39f16e62d1470f910f676b3
Tyg0th <juansvargasv@gmail.com>
2021-04-09 22:28:29 -0500

    add models to shell context

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from flask_migrate import upgrade
 from app import create_app
+from app import db
 import click
 
 from app.api.products.models import Products
@@ -8,6 +9,16 @@ from app.api.orders.models import Orders, OrderDetail
 from app.api.clients.models import Clients
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db,
+    Products=Products,
+    Clients=Clients,
+    Orders=Orders,
+    OrderDetail=OrderDetail
+    )
 
 
 @click.command()
